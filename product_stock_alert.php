cd3e94869514662076f63e970b2468d31b1be626
Promita Ghosh <118420070+user-promi@users.noreply.github.com>
2025-02-26 18:21:51 +0530

    Update product_stock_alert.php

--- product_stock_alert.php
+++ product_stock_alert.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Product Stock Waitlist Manager for WooCommerce
- * Plugin URI: https://multivendorx.com/
+ * Plugin URI: https://notifima.com/
  * Description: Boost sales with real-time stock alerts! Notify customers instantly when products are back in stock. Simplify data management by exporting and importing stock data with ease.
  * Author: MultiVendorX
- * Version: 2.5.16
+ * Version: 2.5.17
  * Requires at least: 5.4
- * Tested up to: 6.7.1
+ * Tested up to: 6.7.2
  * WC requires at least: 8.2.2
- * WC tested up to: 9.6.1
+ * WC tested up to: 9.6.2
  * Author URI: https://multivendorx.com/
  * Text Domain: notifima
  * Requires Plugins: woocommerce
